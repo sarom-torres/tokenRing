3691e2b6f2cd2137608e24c138adff250310486b
saromtorres <saromtorres@gmail.com>
2019-06-09 23:45:16 -0300

    Leitura de mensagem desincronizada

--- tokenRing.ino
+++ tokenRing.ino
@@ -11,17 +11,30 @@ uint32_t countBit = 0; // contador de bits transmitidos
 
 TimerHandle_t xTimerPeriodic = NULL;
 TimerHandle_t xTimerSignal = NULL;
+TimerHandle_t xTimerReader = NULL;
 BaseType_t xTimerSignalStarted = NULL;
 BaseType_t xTimerPeriodicStarted = NULL;
+BaseType_t xTimerReaderStarted = NULL;
 
+void messageGenerator (TimerHandle_t xTimerPeriodic); // CallBack to writer the mensagem
+void startMessage(TimerHandle_t xTimerSignal); //CallBack to initialize the transmission of the message
 
 void setup() {
   Serial.begin(9600);
+
+  //pin
   pinMode(writerPin,OUTPUT);
   digitalWrite(writerPin,HIGH);
-  xTimerPeriodic = xTimerCreate("Frame",1000/ portTICK_PERIOD_MS ,pdTRUE,0,messageGenerator); // Cria um timer periodico para envio da mensagem
-  xTimerSignal = xTimerCreate("Start",1000/portTICK_PERIOD_MS,pdFALSE,0,startMessage); // Cria uma oneShot para iniciar a transmissão da mensagem
-  xTimerSignalStarted = xTimerStart(xTimerSignal,0); // inicia o one_shot
+
+  //timers
+  xTimerPeriodic = xTimerCreate("Frame",1000/ portTICK_PERIOD_MS ,pdTRUE,0,messageGenerator); // Creates a periodic timer for sending de message
+  xTimerSignal = xTimerCreate("Start",1000/portTICK_PERIOD_MS,pdFALSE,0,startMessage); // Creates the oneShot to initialize the transmission of the message
+  xTimerReader = xTimerCreate("Received",1000/ portTICK_PERIOD_MS ,pdTRUE,0,readerMessage); // Creates a periodic timer to reader the message
+  xTimerSignalStarted = xTimerStart(xTimerSignal,0); // Initializes the one_shot
+ 
+
+  //interrupt
+  attachInterrupt(digitalPinToInterrupt(readerPin), messageInterrupt, FALLING);
 }
 
 void loop() {
@@ -34,14 +47,26 @@ void messageGenerator (TimerHandle_t xTimerPeriodic){ //
   else digitalWrite(writerPin,HIGH);
   countBit++;
   if(countBit == 8){
-    xTimerStop(xTimerPeriodic,0); // Verifica se já foram transmitidos 8 bits
-    digitalWrite(writerPin,HIGH);
+    xTimerStop(xTimerPeriodic,0); // Checks if already transmited 8 bits then stop the timer
+    digitalWrite(writerPin,HIGH); //Sets the port in HIGH after the transmission
   }
   Serial.println(countBit);
 }
 
 void startMessage(TimerHandle_t xTimerSignal){
   Serial.println("One Shot");
-  
-  xTimerPeriodicStarted = xTimerStart(xTimerPeriodic,0);// inicia o timer periódico
+  digitalWrite(writerPin,LOW);
+  xTimerPeriodicStarted = xTimerStart(xTimerPeriodic,0);// Initializes the timer
+}
+
+void readerMessage(TimerHandle_t xTimerReader){
+  Serial.println("Reading");
+  if(digitalRead(readerPin) == HIGH) Serial.println("Received HIGH");
+  else Serial.println("Received LOW");
+}
+
+void messageInterrupt(void){
+  detachInterrupt(readerPin);
+  delay(1500);
+  xTimerReaderStarted = xTimerStart(xTimerReader,0);
 }
